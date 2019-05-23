233d5090aac15f945def33eb5e6fae21e05cbf63
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-23 05:58:31 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -48,5 +48,9 @@ WHERE projects.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT projects.category, SUM(pledges.amount) FROM projects
+INNER JOIN pledges
+ON projects.id = pledges.project_id
+GROUP BY projects.category
+HAVING projects.category = 'books';"
 end
