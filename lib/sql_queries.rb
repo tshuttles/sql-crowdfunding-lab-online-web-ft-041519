7ab30de85f947b2717abe7f11a656fd441a300ea
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-23 05:57:37 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,12 @@ ORDER BY projects.title ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT users.name, users.age, SUM(pledges.amount) FROM users
+INNER JOIN pledges
+ON pledges.user_id = users.id
+GROUP BY users.id
+HAVING pledges.user_id = users.id
+ORDER BY users.name ASC"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
